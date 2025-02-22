befbe7c03df13e6932ee8eaaf3c804099b6e1629
Rudraksh <rudrakshkumar2908@gmail.com>
2025-02-22 16:54:22 +0530

    Completed 07_TEMPCONVERSION

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius_to_fahrenheit = (fahrenheit - 32) * (5/9);
+  rounded_off_fahrenheit = Math.round((celcius_to_fahrenheit * 10)) / 10;
+  return rounded_off_fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheit_to_celcius = (celcius * (9/5)) + 32;
+  rounded_off_celcius = Math.round((fahrenheit_to_celcius * 10)) / 10;
+  return rounded_off_celcius;
+
 };
 
 // Do not edit below this line
