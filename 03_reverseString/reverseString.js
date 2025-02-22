50aa0f0b25797af8c0a95d7148803c1bd7f8c3dc
Rudraksh <rudrakshkumar2908@gmail.com>
2025-02-22 14:37:18 +0530

    Completed 03_REVERSESTRING
    PS: It's too long yes but got a little more hands on loops though :)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    splitArr = text.split(" ").reverse();
+    reversed_text = ""
 
+
+    for(index in splitArr){
+        if(index > 0){
+            reversed_text += " ";
+        }
+        letter_length = splitArr[index].length;
+        while(letter_length > 0){
+            reversed_text += splitArr[index][letter_length-1];
+            letter_length -= 1;
+            
+        }
+    }
+    return reversed_text;
 };
 
 // Do not edit below this line
