8ae2ea05e4bbaca170324531cbac76ca994e0f1d
Rudraksh <rudrakshkumar2908@gmail.com>
2025-02-22 16:25:00 +0530

    Completed 05_SUMALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum = 0;
 
+    if(num1 < 0 || num2 < 0 || Number.isInteger(num1) == false || Number.isInteger(num2) == false){
+        return "ERROR";
+    }
+
+    else if(num2 > num1){
+
+        while(num1 <= num2){
+            sum += num1;
+            num1++;
+        }
+        return sum;
+    }
+
+    else{
+
+        while(num1 >= num2){
+            sum += num2
+            num2++
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
