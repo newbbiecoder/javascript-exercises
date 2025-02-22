2db4f892aa2dc002b459bdcde4d380967639709a
Rudraksh <rudrakshkumar2908@gmail.com>
2025-02-22 13:10:16 +0530

    Completed 02_REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text,number){
+    let same_text = text;
 
+    if(text == "" || number == 0){
+        return "";
+    }
+    else if(number < 0){
+        return "ERROR";
+    }
+    for(let num = 1;num < number;num++){         
+            text += same_text;   
+    }
+    return text;
 };
 
 // Do not edit below this line
