f46bb77ab2f68ff7b7c3191cccfe6febb5e33b29
Rudraksh <rudrakshkumar2908@gmail.com>
2025-03-05 20:04:32 +0530

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,43 @@
-const palindromes = function () {
+// split str into array 
+// filter punctuations like comma, dot, !, etc
+// add that into new array
+// reverse the new array
+// check if both equal
 
+const palindromes = function (str) {
+    normal_array = str.split('');
+
+    let new_array = normal_array.filter((item) => {
+        if(item == ' '){
+            return false;
+        }
+        else if(item == '.'){
+            return false;
+        }
+        else if(item == ','){
+            return false;
+        }
+        else if(item == '!'){
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+
+    let reversed_array = new_array.slice();
+
+    reversed_array.reverse();
+
+    let new_array_join = new_array.join('').toLowerCase();
+    let reversed_array_join = reversed_array.join('').toLowerCase();
+
+    if(new_array_join == reversed_array_join){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
