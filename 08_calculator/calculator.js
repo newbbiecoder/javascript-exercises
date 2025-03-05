bd6861050f641d871b670d037ffab6f33234b4ef
Rudraksh <rudrakshkumar2908@gmail.com>
2025-03-05 18:52:25 +0530

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, initial_num) => sum + initial_num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, initial_num) => total * initial_num, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1
+	if(num == 0){
+    return 1;
+  }
+  else{
+    for(let i = 1; i <= num; i++){
+      factorial *= i;
+    }
+    return factorial;
+  }
+
 };
 
 // Do not edit below this line
