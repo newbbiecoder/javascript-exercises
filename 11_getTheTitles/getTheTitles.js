cc987d77023f437dd285efd50144cc9a87d964c7
Rudraksh <rudrakshkumar2908@gmail.com>
2025-03-06 17:47:19 +0530

    Completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+let newObj = [];
+const getTheTitles = function(books) {
+    books.map((item) => newObj.push(item.title));
+    return newObj;
 };
 
 // Do not edit below this line
